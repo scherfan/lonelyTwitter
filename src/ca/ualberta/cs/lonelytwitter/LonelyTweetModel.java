06b5ac9649bb5339444e2bc8c29f480f09c52da0
scherfan <cherfan.ualberta.ca>
2014-09-16 13:23:45 -0600

    added four classes

--- src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
@@ -1,33 +1,41 @@
+
 package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
 
-public abstract class LonelyTweetModel {
+public abstract class LonelyTweetModel
+{
 	private String text;
 	private Date timestamp;
-	public String getText() {
+
+	public String getText()
+	{
 		return text;
 	}
-	
-	public LonelyTweetModel(String text) {
+
+	public LonelyTweetModel(String text)
+	{
 		super();
 		this.text = text;
 		timestamp = new Date();
 	}
-	
-	public LonelyTweetModel(String text, Date timestamp) {
+
+	public LonelyTweetModel(String text, Date timestamp)
+	{
 		super();
 		this.text = text;
 		this.timestamp = timestamp;
 	}
-	
-	public void setText(String text) {
+
+	public void setText(String text)
+	{
 		this.text = text;
 	}
-	
+
 	public abstract Date getTimestamp();
-	
-	public void setTimestamp(Date timestamp) {
+
+	public void setTimestamp(Date timestamp)
+	{
 		this.timestamp = timestamp;
 	}
 }
--- src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,3 +1,4 @@
+
 package ca.ualberta.cs.lonelytwitter;
 
 import java.io.BufferedReader;
@@ -18,76 +19,105 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-public class LonelyTwitterActivity extends Activity {
+public class LonelyTwitterActivity extends Activity
+{
 
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
-	
+
+	// HashSet<String> h;
+
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
 		bodyText = (EditText) findViewById(R.id.body);
-		Button saveButton = (Button) findViewById(R.id.save);
+		// Button saveButton = (Button) findViewById(R.id.save);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
-		saveButton.setOnClickListener(new View.OnClickListener() {
+		// saveButton.setOnClickListener(new View.OnClickListener()
+		// {
+		// });
+	}
 
-			public void onClick(View v) {
-				setResult(RESULT_OK);
-				String text = bodyText.getText().toString();
-				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
-
-			}
-		});
+	public void onClick(View v)
+	{
+		setResult(RESULT_OK);
+		String text = bodyText.getText().toString();
+		saveInFile(text, new Date(System.currentTimeMillis()));
+		finish();
 	}
 
 	@Override
-	protected void onStart() {
+	protected void onStart()
+	{
 		// TODO Auto-generated method stub
 		super.onStart();
 		String[] tweets = loadFromFile();
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				R.layout.list_item, tweets);
+		        R.layout.list_item, tweets);
 		oldTweetsList.setAdapter(adapter);
 	}
 
-	private String[] loadFromFile() {
+	private String[] loadFromFile()
+	{
 		ArrayList<String> tweets = new ArrayList<String>();
-		try {
+		try
+		{
 			FileInputStream fis = openFileInput(FILENAME);
 			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
 			String line = in.readLine();
-			while (line != null) {
+			while (line != null)
+			{
 				tweets.add(line);
 				line = in.readLine();
 			}
+			
+			TweetSubClass1 t1 = new TweetSubClass1("Hi");
+			t1.setText("abc");
+			tweets.add(t1.invertString(t1));
+			
+			TweetSubClass2 t2 = new TweetSubClass2("This is a string");
+			t2.addNum(t2.getText(), t2);
+			tweets.add(t2.getText());
 
-		} catch (FileNotFoundException e) {
+
+			// InterfaceImp1 ii = new InterfaceImp1();
+			// tweets.add(ii.getText());
+		}
+		catch (FileNotFoundException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return tweets.toArray(new String[tweets.size()]);
 	}
-	
-	private void saveInFile(String text, Date date) {
-		try {
-			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + text)
-					.getBytes());
+
+	private void saveInFile(String text, Date date)
+	{
+		try
+		{
+			FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_APPEND);
+			fos.write(new String(date.toString() + " | " + text + "\n")
+			        .getBytes());
 			fos.close();
-		} catch (FileNotFoundException e) {
+		}
+		catch (FileNotFoundException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
