2c0910d9df56b38ec6709e9a44015b8817e6c191
Steven Cherfan <cherfan@ualberta.ca>
2014-10-21 12:14:31 -0600

    Changes made from tutorial

--- lonelyTwitter/src/ca/ualberta/cs/lonelytwitter/IntentReaderActivity.java
+++ lonelyTwitter/src/ca/ualberta/cs/lonelytwitter/IntentReaderActivity.java
@@ -1,3 +1,4 @@
+
 package ca.ualberta.cs.lonelytwitter;
 
 import android.os.Bundle;
@@ -5,7 +6,8 @@ import android.widget.TextView;
 import android.app.Activity;
 import android.content.Intent;
 
-public class IntentReaderActivity extends Activity {
+public class IntentReaderActivity extends Activity
+{
 
 	public static final String TEXT_KEY = "TEXT";
 	public static final String TRANSFORM_KEY = "TRANSFORM";
@@ -13,27 +15,38 @@ public class IntentReaderActivity extends Activity {
 	public static final int NORMAL = 1;
 	public static final int REVERSE = 2;
 	public static final int DOUBLE = 3;
-	
+
 	private String text;
 	private int mode;
-	
-	public String getText() {
+
+	public String getText()
+	{
 		return text;
 	}
-	
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_intent_reader);
+		Intent intent = getIntent();
+		text = intent.getStringExtra(TEXT_KEY);
+		mode = intent.getIntExtra(TRANSFORM_KEY, NORMAL);
+		text = transformText(text);
+		TextView widget = (TextView) findViewById(R.id.intentText);
+		widget.setText(text);
 	}
-	
-	public String transformText(String text) {
-		switch (mode) {
+
+	public String transformText(String text)
+	{
+		switch (mode)
+		{
 			case REVERSE:
-				char[] string =  text.toCharArray();
-				for (int i = 0; i < string.length / 2; i++) {
+				char[] string = text.toCharArray();
+				for (int i = 0; i < string.length / 2; i++)
+				{
 					char tmp = string[i];
-					string[i] = string[string.length - i -1];
+					string[i] = string[string.length - i - 1];
 					string[string.length - i - 1] = tmp;
 				}
 				return new String(string);
---
+++ lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/IntentReaderActivityTest.java
@@ -0,0 +1,84 @@
+
+package ca.ualberta.cs.lonelytwitter.test;
+
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+import ca.ualberta.cs.lonelytwitter.IntentReaderActivity;
+
+public class IntentReaderActivityTest extends
+        ActivityInstrumentationTestCase2<IntentReaderActivity>
+{
+
+	public IntentReaderActivityTest()
+	{
+		super(IntentReaderActivity.class);
+	}
+
+	public void testSendText()
+	{
+		Intent intent = new Intent();
+		intent.putExtra(IntentReaderActivity.TEXT_KEY, "test string");
+		setActivityIntent(intent);
+		IntentReaderActivity activity = (IntentReaderActivity) getActivity();
+		assertEquals("test string", activity.getText());
+	}
+
+	public void testDoubleText()
+	{
+		Intent intent = new Intent();
+		intent.putExtra(IntentReaderActivity.TEXT_KEY, "test string");
+		intent.putExtra(IntentReaderActivity.TRANSFORM_KEY,
+		        IntentReaderActivity.DOUBLE);
+		setActivityIntent(intent);
+		IntentReaderActivity activity = (IntentReaderActivity) getActivity();
+		assertEquals("test stringtest string", activity.getText());
+	}
+
+	public void testDisplayText()
+	{
+		Intent intent = new Intent();
+		intent.putExtra(IntentReaderActivity.TEXT_KEY, "test string");
+		setActivityIntent(intent);
+		IntentReaderActivity activity = (IntentReaderActivity) getActivity();
+		TextView widget = (TextView) activity
+		        .findViewById(ca.ualberta.cs.lonelytwitter.R.id.intentText);
+		assertEquals("test string", widget.getText());
+	}
+
+	public void testDisplayTextDirect() throws Throwable
+	{
+		getActivity();
+		runTestOnUiThread(new Runnable()
+		{
+			
+			@Override
+			public void run()
+			{
+				IntentReaderActivity activity = (IntentReaderActivity) getActivity();
+				TextView widget = (TextView) activity
+				        .findViewById(ca.ualberta.cs.lonelytwitter.R.id.intentText);
+				widget.setText("test string");
+			}
+		});
+		IntentReaderActivity activity = (IntentReaderActivity) getActivity();
+		TextView widget = (TextView) activity.findViewById(ca.ualberta.cs.lonelytwitter.R.id.intentText);
+		assertEquals("test string", widget.getText());
+	}
+	
+	public void testReverseTextCopy()
+    {
+		Intent intent = new Intent();
+		intent.putExtra(IntentReaderActivity.TEXT_KEY, "test string");
+		intent.putExtra(IntentReaderActivity.TRANSFORM_KEY,
+		        IntentReaderActivity.REVERSE);
+		setActivityIntent(intent);
+		IntentReaderActivity activity = (IntentReaderActivity) getActivity();
+		assertEquals("gnirts tset", activity.getText());
+    }
+	
+	public void testReveseText()
+	{
+		
+    }
+}
